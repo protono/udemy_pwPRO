def9423ccf8ecea12f8ec338d5873d44594647c2
Sylvain Pelissier <sylvain@onenetbeyond.org>
2023-10-17 18:47:26 +0200

    new api to test

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:5173',
-    baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:9012',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -72,11 +72,8 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    // command: 'npm run dev',
-    // url: 'http://localhost:5173',
-    command: 'npm run db',
-    url: 'http://localhost:3000/posts',
-    reuseExistingServer: !process.env.CI,
-  },
+  // webServer: {
+  //   // command: 'npm run dev',
+  //   // url: 'http://localhost:5173',
+  // },
 })
--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -1,36 +1,13 @@
 import { expect, test } from '@playwright/test'
 
-test('should be able to delete a comment', async ({ request }) => {
+test('should be able to clear al the items', async ({ request }) => {
+    const erase = await request.patch('/todos/erase')
+    expect(erase).toBeTruthy()
+    expect(erase.status()).toBe(200)
+    expect(await erase.json()).toEqual({ message: 'erased' })
 
-    const postResponse = await request.post('/comments', {
-        data: {
-            body: "body",
-            postId: 1
-        }
-    })
-    expect(postResponse.status()).toBe(201)
-    var response = await postResponse.json()
-    const commentId = response['id']
-    // expect(response['id'] == 2)
-
-    console.log('PUT /comments/' + commentId)
-    const putResponse = await request.put(`/comments/${commentId}`, {
-        data: {
-            body: 'new body ' + commentId,
-            postId: 1
-        }
-    })
-    expect(putResponse.status()).toBe(200)
-    response = await putResponse.json()
-    expect(response['body'] == 'new body' + commentId)
-
-    console.log('DELETE /comments/' + commentId)
-    const deleteResponse = await request.delete(`/comments/${commentId}`)
-    expect(deleteResponse.status()).toBe(200)
-
-    const getResponse = await request.get('/comments')
-    expect(getResponse.status()).toBe(200)
-    response = await getResponse.json()
-    expect(response.length == 1)
-
+    const get = await request.get('/todos')
+    expect(get).toBeTruthy()
+    expect(get.status()).toBe(200)
+    expect(await get.json()).toEqual({ todos: [] })
 })
